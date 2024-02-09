052ca653457c65bcae451349448ec69605065604
Abdullah Enes Can <aenesc1@gmail.com>
2024-02-10 01:16:57 +0300

    Add Contracts project and RabbitMQ configuration

---
+++ src/SearchService/Consumers/AuctionCreatedConsumer.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+
+namespace SearchService.Consumers
+{
+    public class AuctionCreatedConsumer : IConsumer<AuctionCreated>
+    {
+        private readonly IMapper _mapper;
+
+        public AuctionCreatedConsumer(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+        public async Task Consume(ConsumeContext<AuctionCreated> context)
+        {
+            Console.WriteLine($"--> AuctionCreatedConsumer received: {context.Message.Id}");
+
+            var item = _mapper.Map<Item>(context.Message);
+            await item.SaveAsync();
+            
+        }
+    }
+}
--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,14 +1,29 @@
+using MassTransit;
 using Polly;
 using Polly.Extensions.Http;
+using SearchService.Consumers;
 using SearchService.Data;
 using SearchService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddHttpClient<AuctionServiceHttpClient>().AddPolicyHandler(GetPolicy());
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>(); 
+
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search", false));
+    
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.ConfigureEndpoints(context);
+    });
+});
 
 var app = builder.Build();
 
